3ac58dadf52d9c8a2b4b152ce56501c5fabe3e02
Pritam <debnathpritam0802@gmail.com>
2025-09-19 14:27:57 +0530

    connector enhance

--- js/connectors.js
+++ js/connectors.js
@@ -1,13 +1,9 @@
-    
- 
-    
- 
 import { getState, updateState } from "./state.js";
 import { pushHistory } from "./history.js";
 import { draw } from "./drawing.js";
 
 // Create connector
-export function createConnector(aId, bId) {
+export function createConnector(aId, bId, breakPoints) {
   if (!aId || !bId || aId === bId) return null;
 
   const { connectors, connectorIdCounter } = getState();
@@ -17,11 +13,16 @@ export function createConnector(aId, bId) {
   //   (c) => (c.aId === aId && c.bId === bId) || (c.aId === bId && c.bId === aId)
   // );
   const exists = connectors.some(
-     (c) => (c.aId === aId && c.bId === bId) // allow two-way
+    (c) => (c.aId === aId && c.bId === bId) // allow two-way
   );
   if (exists) return null;
 
-  const conn = { id: connectorIdCounter, aId, bId };
+  const conn = {
+    id: connectorIdCounter,
+    aId,
+    bId,
+    breakPoints, // {worldX, worldY}[]
+  };
 
   // Update state
   connectors.push(conn);
@@ -38,6 +39,37 @@ export function hitTestNotes(wx, wy) {
   // return the topmost note (last in array)
   // or null if none hit
   const { notes } = getState();
+
+  for (let i = notes.length - 1; i >= 0; i--) {
+    const n = notes[i];
+    if (wx >= n.x && wx <= n.x + n.w && wy >= n.y && wy <= n.y + n.h) {
+      return n;
+    }
+  }
+  return null;
+  //
+}
+
+export function hitTestConnectorBreakPoint(wx, wy) {
+  // check if point (wx,wy) is inside any note
+  // return the topmost note (last in array)
+  // or null if none hit
+  const { connectors } = getState();
+
+  for (let connector of connectors) {
+    for (let breakPoint of connector.breakPoints) {
+      let { worldX, worldY } = breakPoint
+      let threshold = 8
+      let xCondition = wx > worldX - threshold && wx < worldX + threshold
+      let yCondition = wy > worldY - threshold && wy < worldY + threshold
+      if (
+        xCondition && yCondition
+      ) {
+        return breakPoint
+      }
+    }
+    return null
+  }
 
   for (let i = notes.length - 1; i >= 0; i--) {
     const n = notes[i];
