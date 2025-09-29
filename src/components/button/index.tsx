8c2ee8608ec355e0dccb7af8e9d032e96486cfba
Pritam <debnathpritam0802@gmail.com>
2025-09-29 12:26:57 +0530

    shared component for icon and buttons

---
+++ src/components/button/index.tsx
@@ -0,0 +1,51 @@
+import { Icons } from "../icon"
+
+type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
+    className?: string
+    dataTooltip?: string
+    dataTooltipPos?: "top" | "right" | "bottom" | "left"
+}
+
+
+export const Button = {
+    pan: (props: ButtonProps) => {
+        return (
+            <button id="tool-pan"
+                {...props}
+                data-tooltip={props.dataTooltip || "Pan"}
+                data-tooltip-pos={props.dataTooltipPos || "right"}
+                className={`tool-btn tooltip ${props.className}`}
+            >
+                <Icons.pan />
+            </button>
+        )
+    }
+    ,
+    pen: (props: ButtonProps) => {
+        return (
+            <button id="tool-pen"
+                {...props}
+                className={`tool-btn tooltip ${props.className}`}
+                data-tooltip={props.dataTooltip || "Pen"}
+                data-tooltip-pos={props.dataTooltipPos || "right"}
+            >
+                <Icons.pen />
+            </button>
+        )
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
