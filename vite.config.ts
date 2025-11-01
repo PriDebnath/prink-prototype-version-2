72f7332c18481ddac9acaa073c945bca33b06da6
Pritam <debnathpritam0802@gmail.com>
2025-11-01 11:17:34 +0530

    commented pwa

--- vite.config.ts
+++ vite.config.ts
@@ -10,57 +10,57 @@ export default defineConfig({
   plugins: [
     react(),
     routerPlugin(), // Generate the router code
-    VitePWA({
-      registerType: 'autoUpdate',   // SW auto updates when you deploy a new build
-      includeAssets: ['favicon.ico', 'robots.txt', 'offline.html'],
-      manifest: {
-        name: 'Prink 2.0',
-        short_name: 'Prink 2.0',
-        start_url: './',
-        scope: './',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
-        runtimeCaching: [
-          {
-            // Any runtime requests (like API calls)
-            urlPattern: ({ request }) => request.destination === 'document',
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'html-cache',
-            },
-          },
-          {
-            urlPattern: ({ request }) =>
-              ['style', 'script', 'worker'].includes(request.destination),
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'assets-cache',
-            },
-          },
-          {
-            urlPattern: ({ request }) => request.destination === 'image',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-              },
-            },
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',   // SW auto updates when you deploy a new build
+    //   includeAssets: ['favicon.ico', 'robots.txt', 'offline.html'],
+    //   manifest: {
+    //     name: 'Prink 2.0',
+    //     short_name: 'Prink 2.0',
+    //     start_url: './',
+    //     scope: './',
+    //     display: 'standalone',
+    //     background_color: '#ffffff',
+    //     theme_color: '#000000',
+    //     icons: [
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       }
+    //     ]
+    //   },
+    //   workbox: {
+    //     globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+    //     runtimeCaching: [
+    //       {
+    //         // Any runtime requests (like API calls)
+    //         urlPattern: ({ request }) => request.destination === 'document',
+    //         handler: 'NetworkFirst',
+    //         options: {
+    //           cacheName: 'html-cache',
+    //         },
+    //       },
+    //       {
+    //         urlPattern: ({ request }) =>
+    //           ['style', 'script', 'worker'].includes(request.destination),
+    //         handler: 'StaleWhileRevalidate',
+    //         options: {
+    //           cacheName: 'assets-cache',
+    //         },
+    //       },
+    //       {
+    //         urlPattern: ({ request }) => request.destination === 'image',
+    //         handler: 'CacheFirst',
+    //         options: {
+    //           cacheName: 'images-cache',
+    //           expiration: {
+    //             maxEntries: 50,
+    //             maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+    //           },
+    //         },
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
 })
